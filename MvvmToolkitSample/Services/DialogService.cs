df73b24db5459bb774c2d1d5fb626648e8779a5b
tech-kind <ken_hayakawa@tech-kind.biz>
2022-09-07 17:10:27 +0900

    add services

---
+++ MvvmToolkitSample/Services/DialogService.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Threading.Tasks;
+using MvvmToolkitSample.Core.Services;
+using Windows.UI.Xaml.Controls;
+
+
+#nullable enable
+
+namespace MvvmToolkitSample.Services
+{
+    public sealed class DialogService : IDialogService
+    {
+        public Task ShowMessageDialogAsync(string title, string message)
+        {
+            ContentDialog dialog = new();
+            dialog.Title = title;
+            dialog.CloseButtonText = "Close";
+            dialog.DefaultButton = ContentDialogButton.Close;
+            dialog.Content = message;
+
+            return dialog.ShowAsync().AsTask();
+        }
+    }
+}
---
+++ MvvmToolkitSample/Services/FileService.cs
@@ -0,0 +1,23 @@
+﻿using MvvmToolkitSample.Core.Services;
+using System;
+using System.IO;
+using System.Threading.Tasks;
+using Windows.ApplicationModel;
+using Windows.Storage;
+
+#nullable enable
+
+namespace MvvmToolkitSample.Services
+{
+    public sealed class FileService : IFileService
+    {
+        public string InstallationPath => Package.Current.InstalledLocation.Path;
+
+        public async Task<Stream> OpenForReadAsync(string path)
+        {
+            StorageFile file = await StorageFile.GetFileFromPathAsync(Path.Combine(InstallationPath, path));
+
+            return await file.OpenStreamForReadAsync();
+        }
+    }
+}
