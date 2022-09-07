df03faec98c74078dc797a7bba8d8c66450a965e
tech-kind <ken_hayakawa@tech-kind.biz>
2022-09-07 17:22:55 +0900

    add helpers

---
+++ MvvmToolkitSample/Helpers/TitleBarHelper.cs
@@ -0,0 +1,26 @@
+﻿using Windows.ApplicationModel.Core;
+using Windows.UI;
+using Windows.UI.ViewManagement;
+
+namespace MvvmToolkitSample.Helpers
+{
+    public static class TitleBarHelper
+    {
+        public static void StyleTitleBar()
+        {
+            ApplicationViewTitleBar titleBar = ApplicationView.GetForCurrentView().TitleBar;
+
+            titleBar.ForegroundColor = Colors.Transparent;
+            titleBar.BackgroundColor = Colors.Transparent;
+            titleBar.ButtonBackgroundColor = Colors.Transparent;
+            titleBar.InactiveBackgroundColor = Colors.Transparent;
+            titleBar.ButtonInactiveBackgroundColor = Colors.Transparent;
+        }
+
+        public static void ExpandViewIntoTitleBar()
+        {
+            CoreApplicationViewTitleBar coreTitleBar = CoreApplication.GetCurrentView().TitleBar;
+            coreTitleBar.ExtendViewIntoTitleBar = true;
+        }
+    }
+}
